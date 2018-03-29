d6403a51cb1909c96729cb6a6914f8d559053c3d
xx-l0gik-xx <isaac_lackey@yahoo.com>
2018-03-29 11:42:59 -0700

    actually added indiv lookup

--- app.py
+++ app.py
@@ -23,6 +23,12 @@ def get_tasks():
     return jsonify({'tasks': tasks})
 
 
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
 
 if __name__ == '__main__':
     app.run(debug=True)
